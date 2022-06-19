86696003a19e0257dd1435e0cd397e0c419c219c
Janus Manlapaz <alphaomega030894@gmail.com>
2022-06-18 20:01:08 -0400

    doh

--- components/orders/ui.js
+++ components/orders/ui.js
@@ -13,9 +13,9 @@ export const RPGButton = styled.button`
   background-clip: padding-box;
   background-origin: padding-box;
   background-position: center;
-  background-size: 100% 100%;
+  background-size: 94%;
   /* font size */
-  font-size: 1.0em;
+  font-size: 7px;
   /* default size and display */
   max-width: 100%;
   min-width: 140px;
@@ -34,4 +34,38 @@ export const RPGButton = styled.button`
 
 export const RPGTable = styled(Table)`
   border-image-source: url(/border-image-golden.png);
+  border-image-slice: 4 4 4 4;
+  -moz-box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  background: url(/background-image-golden.png) center;
+  background-size: 60%;
+  background-clip: padding-box;
+  border-width: 14px;
+  border-style: solid;
+  background-origin: padding-box;
+  font-family: 'Press Start 2P',cursive;
+  color: white;
+
+  .ant-table-thead > tr > th {
+    position: relative;
+    color: rgba(0, 0, 0, 0.85);
+    font-weight: 500;
+    text-align: left;
+    background: #fafafa;
+    border-bottom: 1px solid #f0f0f0;
+    background: url(/background-image-golden.png) center;
+    transition: background 0.3s ease;
+    color: white;
+  }
+  .ant-table-tbody > tr > td {
+    border-bottom: 1px solid #f0f0f0;
+    transition: background 0.3s;
+    background: url(/background-image-golden.png) center;
+    color: white;
+  }
+
+  .ant-table-tbody > tr.ant-table-row:hover > td, .ant-table-tbody > tr > td.ant-table-cell-row-hover {
+    background: url(/background-image-golden.png) center;
+    color: white;
+  }
 `
